ec39e696e0f913ba74eea6257aa3f6e300c470a0
FRANCISCO DOMINGUEZ NAVARRO <47344051@ort>
2023-07-06 15:54:33 -0300

    Modificacion 6/07

--- Models/BD.cs
+++ Models/BD.cs
@@ -1,67 +1,80 @@
+using System.Data.SqlClient;
+using Dapper;
+
 public static class BD
 {
-    private static List<Candidato> candidatos = new List<Candidato>();
+
+    private static string _ConnectionString = @"Server=localhost; DataBase=NombreBase; Trusted_Connection=True;";
+
     public static void AgregarCandidato(Candidato can)
     {
-        candidatos.Add(can);
+        string SQL = "INSERT INTO Candidato (IdPartido, Apellido, Nombre, FechaNacimiento, Foto, Postulacion) VALUES (@pIdPartido, @pApellido, @pNombre, @pFechaNacimiento, @pFoto, @pPostulacion)";
+        using (SqlConnection db = new SqlConnection(_ConnectionString))
+        {
+            db.Execute(SQL, new {pIdPartido = can.IdPartido, pApellido = can.Apellido, pNombre = can.Nombre, pFechaNacimiento = can.FechaNacimiento, pFoto = can.Foto, pPostulacion = can.Postulacion});
+        }
     }
 
-    public static void EliminarCandidato(int idCandidato)
+    public static int EliminarCandidato(int idCandidato)
     {
-        Candidato candidato = candidatos.FirstOrDefault(c => c.IdCandidato == idCandidato);
-
-        if(candidato != null)
-        {
-            candidatos.Remove(candidato);
-        }
+       int RegistrosEliminados = 0;
+       string sql = "DELETE FROM Candidato WHERE IdCandidato = @IdCandidato";
+       using (SqlConnection db = new SqlConnection(_ConnectionString))
+       {
+            RegistrosEliminados = db.Execute(sql, new {IdCandidato = idCandidato});        
+       }
+       return RegistrosEliminados;
     }
 
     public static Partido VerInfoPartido(int idPartido)
     {
-        foreach (Partido partido in partidos)
+        Partido MiPartido = null;
+        using (SqlConnection db = new SqlConnection(_ConnectionString))
         {
-            if (partido.IdPartido == idPartido)
-            {
-                return partido;
-            }
+            string sql = "SELECT * FROM Partido WHERE IdPartido = @pIdPartido";
+            MiPartido = db.QueryFirstOrDefault<Partido>(sql, new { pIdPartido = idPartido});
         }
-
-        return null;
+        return MiPartido;
     }
 
     public static Candidato VerInfoCandidato(int idCandidato)
     {
-        foreach (Candidato candidato in candidatos)
+        Candidato MiCandidato = null;
+        using (SqlConnection db = new SqlConnection(_ConnectionString))
         {
-            if (candidato.IdCandidato == idCandidato)
-            {
-                return candidato;
-            }
+            string sql = "Select * FROM Candidato WHERE IdCandidato = @pIdCandidato";
+            MiCandidato = db.QueryFirstOrDefault<Candidato>(sql, new { pIdCandidato = idCandidato});
         }
-
-        return null;
+        return MiCandidato;
     }
 
-    private static List<Partido> partidos = new List<Partido>();
+    private static List<Partido> _ListadoPartidos = new List<Partido>();
 
     public static List<Partido> ListarPartidos()
     {
-        return partidos;
+        using (SqlConnection db = new SqlConnection(_ConnectionString))
+        {
+            string sql = "SELECT * FROM Partido";
+            _ListadoPartidos = db.Query<Partido>(sql).ToList();
+        }
+        return _ListadoPartidos;
     } 
 
+    private static List<Candidato> _ListadoCandidatos = new List<Candidato>();
     public static List<Candidato> ListarCandidatos(int idPartido)
     {
-        List<Candidato> candidatosPartido = new List<Candidato>();
-
-        foreach (Candidato candidato in candidatos)
+        using (SqlConnection db = new SqlConnection(_ConnectionString))
         {
-            if (candidato.IdPartido == idPartido)
-            {
-                candidatosPartido.Add(candidato);
-            }
+            string sql = "SELECT * FROM Candidato WHERE IdPartido = @pIdPartido";
+            _ListadoCandidatos = db.Query<Candidato>(sql, new { pIdPartido = idPartido}).ToList();
         }
-
-        return candidatosPartido;
+        return _ListadoCandidatos;
     }
 
 }
+
+
+
+
+
+    
